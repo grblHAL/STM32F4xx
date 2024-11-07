ae6abede029e1174bf6385beb98ae149b1f85295
Terje Io <terjeio@users.noreply.github.com>
2024-11-07 20:58:32 +0100

    Updated for removal of deprecated stream flags in the core.

--- Inc/usb_serial.h
+++ Inc/usb_serial.h
@@ -4,20 +4,20 @@
 
   Part of grblHAL
 
-  Copyright (c) 2021-2023 Terje Io
+  Copyright (c) 2021-2024 Terje Io
 
-  Grbl is free software: you can redistribute it and/or modify
+  grblHAL is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  Grbl is distributed in the hope that it will be useful,
+  grblHAL is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+  along with grblHAL. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
@@ -27,15 +27,6 @@
 #include <stdbool.h>
 
 #include "driver.h"
-
-typedef union {
-    uint8_t value;
-    struct {
-        uint8_t dtr    :1,
-                rts    :1,
-                unused :6;
-    };
-} serial_linestate_t;
 
 typedef struct {
     serial_linestate_t pin;
