7ba60931db26ba184e2ca70892c2867385049434
Terje Io <terjeio@users.noreply.github.com>
2025-10-17 07:34:19 +0200

    Added STM32F446ZETX loader script

--- Inc/driver.h
+++ Inc/driver.h
@@ -165,6 +165,8 @@
   #include "boards/btt_skr_2.0_map.h"
 #elif defined(BOARD_BTT_SKR_20_DAC)
   #include "boards/btt_skr_2.0_dac_map.h"
+#elif defined(BOARD_BTT_OCTOPUS_PRO)
+  #include "boards/btt_octopus_pro_map.h"
 #elif defined(BOARD_FYSETC_S6)
   #include "boards/fysetc_s6_map.h"
 #elif defined(BOARD_PROTONEER_3XX)
